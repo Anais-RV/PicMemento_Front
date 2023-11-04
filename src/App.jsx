da8961fc08c4a4a1ae709ce963e22333cdff6d13
Anais-RV <anaisrv@gmail.com>
2023-11-04 20:16:59 +0100

    Router.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import RouterItem from "./routes/Router";
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <RouterItem/>
+    </>
+  )
+}
+
+export default App
+
---
+++ src/routes/router.jsx
@@ -0,0 +1,17 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layout from '../layouts/Homelayout/Layout';
+
+
+
+
+const RouterItem = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+            <Route path='/' element={<Layout/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RouterItem;
