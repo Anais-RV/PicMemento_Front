d5677912e0b4ba3e039fa91bd5a5ca0a9d5c97da
Anais-RV <anaisrv@gmail.com>
2023-11-07 13:03:22 +0100

    solved put petition

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,96 +1,3 @@
-// import { useState } from "react";
-// import "./Card.css";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import PropTypes from "prop-types";
-// import { faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
-
-// const API_URL = "http://localhost:8000";
-
-// function Card(props) {
-//   const [isEditing, setIsEditing] = useState(false);
-//   const [editedText, setEditedText] = useState(props.text);
-
-//   const deleteImage = async (imageId) => {
-//     const userConfirmed = window.confirm(
-//       "¿Estás seguro de que deseas eliminar la imagen para siempre?"
-//     );
-//     if (!userConfirmed) return;
-
-//     try {
-//       const response = await fetch(API_URL + imageId, { method: "DELETE" });
-//       if (!response.ok) throw new Error("Failed to delete image");
-//       window.location.reload();
-//     } catch (error) {
-//       console.error("Error eliminando la imagen:", error);
-//     }
-//   };
-
-//   const handleEditClick = () => {
-//     setIsEditing(true);
-//   };
-
-//   const handleSaveClick = async () => {
-//     try {
-//       // Realiza la solicitud para guardar el título editado en el servidor
-//       const response = await fetch(API_URL + props.imageId, {
-//         method: "PUT",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ text: editedText }),
-//       });
-
-//       if (!response.ok) {
-//         throw new Error("Failed to save image title");
-//       }
-
-//       setIsEditing(false);
-//     } catch (error) {
-//       console.error("Error al guardar el título:", error);
-//     }
-//   };
-
-//   const handleTitleChange = (event) => {
-//     setEditedText(event.target.value);
-//   };
-
-//   const renderIcon = (icon, action) => (
-//     <figure className={`icon-pointer ${icon}-icon`} onClick={action}>
-//       <FontAwesomeIcon icon={icon} size="xl" />
-//     </figure>
-//   );
-
-//   return (
-//     <div className="card">
-//       <img className="cardImage" src={props.img_card} alt={props.text} />
-//       {isEditing ? (
-//         <div>
-//           <input className="edit"
-//             type="text"
-//             value={editedText}
-//             onChange={handleTitleChange}
-//           />
-//           <button className="save" onClick={handleSaveClick}>Guardar</button>
-//         </div>
-//       ) : (
-//         <div className="imageText">{editedText}</div>
-//       )}
-//       <div className="cardIcons">
-//         {renderIcon(faTrash, () => deleteImage(props.imageId))}
-//         {renderIcon(faEdit, handleEditClick)}
-//       </div>
-//     </div>
-//   );
-// }
-
-// Card.propTypes = {
-//   img_card: PropTypes.string,
-//   text: PropTypes.string,
-//   imageId: PropTypes.number,
-// };
-
-// export default Card;
-
 import { useState } from "react";
 import "./Card.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -100,6 +7,7 @@ import { faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
 const API_URL = "http://localhost:8000";
 
 function Card(props) {
+  console.log("Valor de props.imageId:", props.imageId);
   const [isEditing, setIsEditing] = useState(false);
   const [editedText, setEditedText] = useState(props.text);
 
@@ -127,25 +35,37 @@ function Card(props) {
   };
 
   const handleSaveClick = async () => {
-    try {
-      // Realiza la solicitud para guardar el título editado en el servidor
-      const response = await fetch(`${API_URL}/images/${props.imageId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ title: editedText }),
-      });
+    console.log("Guardar botón clicado");
+    console.log("ID de imagen:", props.imageId);
+  
+    const url = `${API_URL}/images/${props.imageId}`; // Construye la URL
+    console.log("URL de la solicitud:", url); // Muestra la URL en la consola
 
-      if (!response.ok) {
+    const formData = new FormData();
+    formData.append('title', editedText);
+
+    const requestOptions = {
+      method: "PUT",
+      body: formData,
+    };
+
+    try {
+      const response = await fetch(url, requestOptions); // Utiliza el formData en el cuerpo
+
+      if (response.ok) {
+        setIsEditing(false); // Cambia al modo de visualización
+      } else {
         throw new Error("Failed to save image title");
       }
-
-      setIsEditing(false);
     } catch (error) {
       console.error("Error al guardar el título:", error);
     }
-  };
+};
+
+
+
+  
+  
 
   const handleTitleChange = (event) => {
     setEditedText(event.target.value);
@@ -184,9 +104,9 @@ function Card(props) {
       }
 
 Card.propTypes = {
-  img_card: PropTypes.string, // Asegúrate de que este prop contenga la URL de la imagen
+  img_card: PropTypes.string, 
   text: PropTypes.string,
   imageId: PropTypes.number,
-  setImageList: PropTypes.func, // Propiedad para actualizar la lista de imágenes en Landing
+  setImageList: PropTypes.func, 
 };
 export default Card;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -73,7 +73,110 @@
 
 // export default Landing;
 
-import { useState } from 'react';
+// import { useState } from 'react';
+// import { toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import Card from '../components/Card/Card';
+// import './Landing.css';
+
+// const API_URL = "http://localhost:8000";
+
+// function Landing() {
+//   const [selectedFile, setSelectedFile] = useState(null);
+//   const [title, setTitle] = useState('');
+//   const [imageList, setImageList] = useState([]);
+
+//   const handleFileChange = (e) => {
+//     const file = e.target.files[0];
+//     if (file) {
+//       setSelectedFile(file);
+//     }
+//   };
+
+//   const handleTitleChange = (e) => {
+//     setTitle(e.target.value);
+//   };
+
+//   const handleUpload = async () => {
+//     if (selectedFile && title) {
+//       try {
+//         const formData = new FormData();
+//         formData.append('image', selectedFile);
+//         formData.append('title', title);
+  
+//         const response = await fetch(`${API_URL}/images`, {
+//           method: 'POST',
+//           body: formData,
+//         });
+  
+//         if (response.ok) {
+//           const newImage = await response.json();
+//           // Asegúrate de incluir la propiedad 'img_card' con la URL de la imagen
+//           newImage.img_card = newImage.image_url;
+  
+//           // Agrega el objeto de imagen a la lista
+//           setImageList([...imageList, newImage]);
+//           setSelectedFile(null);
+//           setTitle('');
+//           toast.success('Imagen añadida con éxito', {
+//             position: 'bottom-right',
+//             autoClose: 3000,
+//           });
+//         } else {
+//           throw new Error('Failed to upload image');
+//         }
+//       } catch (error) {
+//         console.error('Error al cargar la imagen:', error);
+//       }
+//     } else {
+//       toast.error('Por favor, seleccione una imagen y añada un título');
+//   }
+//   }
+
+//   return (
+//     <div className="image-uploader-container">
+//       <div className="upload-form">
+//         <div className="form-row">
+//           <label className="file-upload-label" htmlFor="fileInput">
+//             Elige tu recuerdo
+//           </label>
+//           <input
+//             type="file"
+//             id="fileInput"
+//             onChange={handleFileChange}
+//             required
+//           />
+//         </div>
+//         <div className="form-row">
+//           <input
+//             type="text"
+//             placeholder="Escoge un título"
+//             value={title}
+//             onChange={handleTitleChange}
+//           />
+//           <button onClick={handleUpload}>Guardar</button>
+//         </div>
+//       </div>
+//       <div className="card-container">
+//   {imageList.map((image) => (
+//     <Card
+//       key={image.id}
+//       img_card={image.image_url}
+//       text={image.title}
+//       imageId={image.id}
+//       setImageList={setImageList} 
+//     />
+//   ))}
+// </div>
+
+//     </div>
+//   );
+// }
+
+// export default Landing;
+
+
+import { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Card from '../components/Card/Card';
@@ -85,6 +188,27 @@ function Landing() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [title, setTitle] = useState('');
   const [imageList, setImageList] = useState([]);
+
+  // Agregar un efecto para cargar imágenes al montar el componente
+  useEffect(() => {
+    // Realizar una solicitud GET al servidor para cargar todas las imágenes disponibles
+    const loadImages = async () => {
+      try {
+        const response = await fetch(`${API_URL}/images`);
+        if (response.ok) {
+          const images = await response.json();
+          // Actualizar la lista de imágenes
+          setImageList(images);
+        } else {
+          throw new Error('Failed to load images');
+        }
+      } catch (error) {
+        console.error('Error al cargar las imágenes:', error);
+      }
+    };
+
+    loadImages(); // Llamar a la función para cargar imágenes al montar el componente
+  }, []);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -103,18 +227,16 @@ function Landing() {
         const formData = new FormData();
         formData.append('image', selectedFile);
         formData.append('title', title);
-  
+
         const response = await fetch(`${API_URL}/images`, {
           method: 'POST',
           body: formData,
         });
-  
+
         if (response.ok) {
           const newImage = await response.json();
-          // Asegúrate de incluir la propiedad 'img_card' con la URL de la imagen
           newImage.img_card = newImage.image_url;
-  
-          // Agrega el objeto de imagen a la lista
+
           setImageList([...imageList, newImage]);
           setSelectedFile(null);
           setTitle('');
@@ -130,7 +252,7 @@ function Landing() {
       }
     } else {
       toast.error('Por favor, seleccione una imagen y añada un título');
-  }
+    }
   }
 
   return (
@@ -158,17 +280,16 @@ function Landing() {
         </div>
       </div>
       <div className="card-container">
-  {imageList.map((image) => (
-    <Card
-      key={image.id}
-      img_card={image.image_url}
-      text={image.title}
-      imageId={image.id}
-      setImageList={setImageList} 
-    />
-  ))}
-</div>
-
+        {imageList.map((image) => (
+          <Card
+            key={image.id}
+            img_card={image.image_url}
+            text={image.title}
+            imageId={image.id}
+            setImageList={setImageList}
+          />
+        ))}
+      </div>
     </div>
   );
 }
